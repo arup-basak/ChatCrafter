9eeef74aa7c24aef610854b325ebd0a5c94eb292
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-05-19 00:20:46 +0530

    improve app.js

--- app.js
+++ app.js
@@ -14,18 +14,27 @@ const cache = {
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Route for the homepage
-app.get('/', (req, res) => {
+// Middleware to check login status
+const checkLogin = (req, res, next) => {
   const data = myCache.get('userkey');
-  if (data.username > 0 && data.password > 0) {
-    // Login Check
-    res.redirect('/chat')
+  if (data.username.length > 0 && data.password.length > 0) {
+    next(); // User logged in, proceed to the next middleware/route handler
+  } else {
+    res.redirect('/login'); // User not logged in, redirect to login page
   }
-  res.sendFile(__dirname + '/public/login.html');
+};
+
+// Route for the homepage
+app.get('/', checkLogin, (req, res) => {
+  res.redirect('/chat');
 });
 
-app.get('/chat', (req, res) => {
+app.get('/chat', checkLogin, (req, res) => {
   res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
 });
 
 app.get('/savecache', (req, res) => {
@@ -34,38 +43,40 @@ app.get('/savecache', (req, res) => {
     cache.username = username;
     cache.password = password;
     myCache.set('userkey', cache, 2592000);
-    res.redirect('/chat')
+    res.redirect('/chat');
+  } else {
+    res.redirect('/login');
   }
-  req.redirect('/login')
-})
+});
 
 app.get('/whoami', (req, res) => {
-  res.json({ user: myCache.get('userkey') })
-})
+  res.json({ user: myCache.get('userkey') });
+});
 
 app.get('/chatdata', async (req, res) => {
   try {
     const chatRooms = await DBHelper.getAllChatRooms();
-    res.json(chatRooms)
-  }
-  catch {
+    res.json(chatRooms);
+  } catch (error) {
     res.status(500).json({ error: 'Failed to get chat rooms.' });
   }
 });
 
-// get total chat as JSON whan 
 app.get('/getchat', async (req, res) => {
   const { room } = req.query;
-  const chats = await DBHelper.getAllChats()
-  const chat = chats.find(item => item.room === room)
+  try {
+    const chats = await DBHelper.getAllChats();
+    const chat = chats.find(item => item.room === room);
 
-  if (chat) {
-    res.json(chat);
-  } else {
-    res.status(404).json({ error: "Chat not found" });
+    if (chat) {
+      res.json(chat);
+    } else {
+      res.status(404).json({ error: "Chat not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get chat.' });
   }
 });
-
 
 // Socket.IO connection event
 io.on('connection', (socket) => {
@@ -77,12 +88,12 @@ io.on('connection', (socket) => {
     roomsArr.forEach((room) => {
       socket.join(room)
     });
-  })
+  });
 
   // Handle chat message event
   socket.on('chat message', (msg) => {
-    console.log(msg)
-    io.to(msg.room).emit("message-to-user", msg)
+    console.log(msg);
+    io.to(msg.room).emit("message-to-user", msg);
   });
 
   // Handle disconnect event
